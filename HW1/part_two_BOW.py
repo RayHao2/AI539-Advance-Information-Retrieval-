f20262133660704edf27e2d1ea7ac8157a82da9f
Hao Jun Chen <haojunchen51@gmail.com>
2024-05-08 11:06:20 -0700

    SP

--- HW1/part_two_BOW.py
+++ HW1/part_two_BOW.py
@@ -20,6 +20,7 @@ from gensim.models import TfidfModel
 from gensim.corpora import Dictionary
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
+
 def is_integer(s):
     try:
         int(s)
@@ -33,14 +34,15 @@ def remove_non_alphanumeric(input_string):
     result = re.sub(r'[^a-zA-Z0-9]', '', input_string)
     return result
 
-def Process_doc_get_all_words():
+
+def get_vocab():
     current_dir = os.getcwd()
     path = os.path.join(os.path.dirname(current_dir), "yelp")
     stemmer = PorterStemmer()
     yelp_store_list = os.listdir(path)
     nltk.download('stopwords')
     stop_words = set(stopwords.words('english'))
-
+    vocabulary = set()
     # Prepare documents
     for yelp_store in yelp_store_list:
         store_path = path + "/" + yelp_store
@@ -63,3 +65,144 @@ def Process_doc_get_all_words():
                 else:
                     token = stemmer.stem(token)
                     token = token.lower()
+                vocabulary.add(token)
+    vocabulary = sorted(vocabulary)
+    with open("pklSave/vocabulary.pkl", "wb") as f:
+        pickle.dump(vocabulary, f)
+    vocabulary_index = {word: i for i, word in enumerate(vocabulary)}
+    with open("pklSave/vocabulary_index.pkl", "wb") as f:
+        pickle.dump(vocabulary_index, f)
+
+
+def process_doc():
+    current_dir = os.getcwd()
+    path = os.path.join(os.path.dirname(current_dir), "yelp")
+    stemmer = PorterStemmer()
+    yelp_store_list = os.listdir(path)
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
+    reviewsContents = {}
+    # Prepare documents
+    for yelp_store in yelp_store_list:
+        store_path = path + "/" + yelp_store
+        with open(store_path, 'r') as file:
+            data = json.load(file)
+        reviews = data["Reviews"]
+        # Loop through each review in the yelp _ file
+        for review in reviews:
+            doc = review['Content']
+            reviewID = review['ReviewID']
+            processed_review = []
+            tokens = word_tokenize(doc)  # token the review content
+            # Loop through each token and normal it
+            for token in tokens:
+                token = remove_non_alphanumeric(token)
+                if len(token) == 0:
+                    continue
+                elif token in stop_words:
+                    continue
+                elif is_integer(token):
+                    token = "NUM"
+                else:
+                    token = stemmer.stem(token)
+                    token = token.lower()
+                processed_review.append(token)
+            reviewsContents[reviewID] = processed_review
+
+    with open("pklSave/reviewsContents.pkl", "wb") as f:
+        pickle.dump(reviewsContents, f)
+
+
+def BOW(vocab_index):
+    current_dir = os.getcwd()
+    path = os.path.join(os.path.dirname(current_dir), "yelp")
+    stemmer = PorterStemmer()
+    yelp_store_list = os.listdir(path)
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
+    BOWs = []
+    # Prepare documents
+    for yelp_store in yelp_store_list:
+        store_path = path + "/" + yelp_store
+        with open(store_path, 'r') as file:
+            data = json.load(file)
+        reviews = data["Reviews"]
+        # Loop through each review in the yelp _ file
+        for review in reviews:
+            doc = review['Content']
+            reviewID = review['ReviewID']
+            BOW = set()
+            tokens = word_tokenize(doc)  # token the review content
+            # Loop through each token and normal it
+            for token in tokens:
+                token = remove_non_alphanumeric(token)
+                if len(token) == 0:
+                    continue
+                elif token in stop_words:
+                    continue
+                elif is_integer(token):
+                    token = "NUM"
+                else:
+                    token = stemmer.stem(token)
+                    token = token.lower()
+                BOW.add(vocab_index[token])
+            BOW_tuple = (reviewID, BOW)
+            BOWs.append(BOW_tuple)
+
+    with open("pklSave/BOWs.pkl", "wb") as f:
+        pickle.dump(BOWs, f)
+
+
+def process_query(vocab_index, vocab):
+    with open("pklSave/query.pkl", "rb") as f:
+        query = pickle.load(f)
+    # constrct query index
+    query_index = []
+    for q in query:
+        query_set = set()
+        for word in q:
+            index = vocab_index[word]
+            query_set.add(index)
+        query_index.append(query_set)
+
+    for index in query_index:
+        for i in index:
+            print(vocab[i], end=" ")
+        print()
+
+    with open("pklSave/query_index.pkl", "wb") as f:
+        pickle.dump(query_index, f)
+
+def look_up():
+    pass
+
+def main():
+    # get_vocab()
+    with open("pklSave/vocabulary.pkl", "rb") as f:
+        vocab = pickle.load(f)
+    with open("pklSave/vocabulary_index.pkl", "rb") as f:
+        vocab_index = pickle.load(f)
+
+    # process_doc()
+    with open("pklSave/reviewsContents.pkl", "rb") as f:
+        reviewsContents = pickle.load(f)
+        # for review in reviewsContents:
+        #     print(reviewsContents[review])
+        #     break
+
+    # BOW(vocab_index)
+    with open("pklSave/BOWs.pkl", "rb") as f:
+        BOWs = pickle.load(f)
+        # for index in BOWs[0][1]:
+        #     print(vocab[index], sep=" ")
+
+    # process_query(vocab_index, vocab)
+    with open("pklSave/query_index.pkl", "rb") as f:
+        query_index = pickle.load(f)
+        print(query_index)
+        
+    look_up(vocab, vocab_index, reviewsContents, BOWs, query_index)
+
+
+if __name__ == "__main__":
+    main()
